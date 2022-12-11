3a3fa682a5e781fddf703e8748e505613e1a3603
SarthakKl <sarthakk60@gmail.com>
2022-12-11 17:48:24 +0530

    changing backend url

--- client/src/utils/api/api_call.js
+++ client/src/utils/api/api_call.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://stage1env-env.eba-8rpns27j.us-east-1.elasticbeanstalk.com/'
-axios.defaults.baseURL = "https://7310-116-193-143-52.ngrok.io/"
+axios.defaults.baseURL = "https://ipr-management-system-69.vercel.app/"
 
 export const postApi = async({url,data,params,headers})=>{
   console.log("Api Called")
